2f53310300c00fb09605a24fed47d4e3b66bf6cf
Rene Mejia <rene.mejiac@gmail.com>
2022-12-29 23:38:48 -0800

    + Emojis ✨

--- juego.py
+++ juego.py
@@ -9,35 +9,35 @@ user_option = input('Juguemos! Elige piedra, papel o tijera => ')
 user_option = user_option.lower()
 
 if user_option == computer_option:
-    print(f"La computadora ha elegido {computer_option}")
-    print("Es un empate")
+    print(f"Tu rival ha elegido {computer_option}")
+    print("Es un empate 😬")
 
 elif user_option == 'piedra':
     if computer_option == 'tijera':
-        print(f'La computadora eligió {computer_option}')
+        print(f'Tu rival eligió {computer_option}')
         print('Piedra gana a tijera')
-        print('Has ganado!')
+        print('Has ganado! 🎉')
     else:
-        print(f'La computadora ha elegido {computer_option}')
+        print(f'Tu rival ha elegido {computer_option}')
         print('Papel gana a piedra')
-        print('La computadora ha ganado')
+        print('Tu rival ha ganado 🫠')
 
 elif user_option == 'papel':
     if computer_option == 'piedra':
-        print(f'La computadora eligió {computer_option}')
+        print(f'Tu rival eligió {computer_option}')
         print('Papel gana a piedra')
-        print('Has ganado!')
+        print('Has ganado! 🎉')
     else:
-        print(f'La computadora eligió {computer_option}')
+        print(f'Tu rival eligió {computer_option}')
         print('Tijera gana a papel')
-        print('La computadora ha ganado')
+        print('Tu rival ha ganado 🫠')
 
 elif user_option == 'tijera':
     if computer_option == 'piedra':
-        print(f'La computadora eligió {computer_option}')
+        print(f'Tu rival eligió {computer_option}')
         print('Piedra gana a tijera')
-        print('La computadora ha ganado')
+        print('Tu rival ha ganado 🫠')
     else:
-        print(f'La computadora eligió {computer_option}')
+        print(f'Tu rival eligió {computer_option}')
         print('Tijera gana a papel')
-        print('Has ganado!')
+        print('Has ganado! 🎉')

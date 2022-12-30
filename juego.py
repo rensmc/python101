1193004592463bc5d5352733d3f250e6e1a8cd32
Rene Mejia <renemejia@Renes-MacBook-Air.local>
2022-12-29 22:24:27 -0800

    + juego de piedra papel o tijera

---
+++ juego.py
@@ -0,0 +1,42 @@
+# Juego de piedra, papel o tijera
+
+import random
+
+options = ['piedra', 'papel', 'tijera']
+computer_option = random.choice(options)
+
+user_option = input('Juguemos! Elige piedra, papel o tijera => ')
+
+if user_option == computer_option:
+    print(f"La computadora ha elegido {computer_option}")
+    print("Es un empate")
+
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print(f'La computadora eligió {computer_option}')
+        print('Piedra gana a tijera')
+        print('Has ganado!')
+    else:
+        print(f'La computadora ha elegido {computer_option}')
+        print('Papel gana a piedra')
+        print('La computadora ha ganado')
+
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print(f'La computadora eligió {computer_option}')
+        print('Papel gana a piedra')
+        print('Has ganado!')
+    else:
+        print(f'La computadora eligió {computer_option}')
+        print('Tijera gana a papel')
+        print('La computadora ha ganado')
+
+elif user_option == 'tijera':
+    if computer_option == 'piedra':
+        print(f'La computadora eligió {computer_option}')
+        print('Piedra gana a tijera')
+        print('La computadora ha ganado')
+    else:
+        print(f'La computadora eligió {computer_option}')
+        print('Tijera gana a papel')
+        print('Has ganado!')

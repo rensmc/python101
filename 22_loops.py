e84cad4a2a2262f12a60827a07c0497a810f8fe5
Rene Mejia <rene.mejiac@gmail.com>
2023-01-01 16:35:33 -0800

    + nested loops

--- 22_loops.py
+++ 22_loops.py
@@ -1,1 +1,17 @@
 print('🎉 Happy new year! ✨')
+
+matriz = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9]
+    ]
+
+print(matriz[0]) #[1, 2, 3]
+print(matriz[0][1]) #2
+
+#Ciclos anidados
+
+for row in matriz:
+    print(row)
+    for column in row:
+        print(column)
